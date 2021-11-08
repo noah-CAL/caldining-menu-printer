eac6d8390f82f4cd05fa97dbbf23725a57869237
noahdev4 <noahdev4@gmail.com>
2021-11-08 12:31:52 -0800

    add test code

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+from types import FunctionType
 import bs4
 from bs4 import BeautifulSoup
 
@@ -8,14 +9,55 @@ class HTMLElement:
         self.soup = soup
         self.text = soup.find('span').get_text()
     
+    def get_soup(self):
+        return self.soup
+    
+    def get_text(self):
+        return self.text
+
     def __str__(self):
         return self.text
 
 class HTMLCollection:
     """Data Abstraction that contains groups of HTMLElements of a specific type (i.e. DiningHalls, Menus, etc.)"""
-    def __init__(self, soup, tag, class_):
+    def __init__(self, soup, tag, class_, key: FunctionType=None):
+        """Creats a container of HTMLElements for which key(HTMLelement) is true (or key=None by default)"""
         assert isinstance(soup, bs4.element.Tag), f'soup {soup} must be of type soup!'
-        self.collection = [HTMLElement(el) for el in soup.find_all(tag, class_=class_)]
+        self.collection = list(filter(key, [HTMLElement(el) for el in soup.find_all(tag, class_=class_)]))
     
+    def get_elements(self):
+        return self.collection
+
     def __str__(self):
         return '\n'.join([str(el) for el in self.collection])
+    
+    def __iter__(self):
+        return iter(self.collection)
+    
+    BANNED_STATIONS = [
+        # CAFE 3
+        'HOT CEREAL',
+        'PASTRIES',
+        'FRUIT',
+        'KOSHER DELI',
+        'KOSHER DELI (Per Request)',
+        'SALADS',
+        'DESSERT',
+        'SOUPS',
+        # CLARK KERR CAMPUS
+        'SOUP & SALAD'
+        # CROSSROADS
+        'HOT MORNING GRAINS',
+        'BREAKFAST BREAD',
+        'DAILY RICE',
+        'BEAR FIT',
+        'SALADS',
+        'DELI',
+        # FOOTHILL
+        'DAILY RICE',
+        'DAILY HOT GRAINS',
+        'FIT BOWL',
+        'DELI',
+        'FIT BOWL',
+        'MAIN LINE VEGGIE',
+    ]
--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-## SETUP ##
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,18 +6,32 @@ from classes import HTMLCollection
 
 URL = 'https://caldining.berkeley.edu/menus/'
 
-if __name__ == '__main__':
-    with open('example.html', 'r') as f:
-        soup = BeautifulSoup(f, 'html.parser')
-else:
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
+## SETUP TEST CODE ##
+# if __name__ == '__main__':
+#     with open('example.html', 'r') as f:
+#         soup = BeautifulSoup(f, 'html.parser')
+# else:
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
 ## END SETUP ##
 
 def setup(url=URL, test=False):
     dining_hall_collection = HTMLCollection(soup, 'li', 'location-name') 
-    print(dining_hall_collection)
+    for dining_hall in dining_hall_collection:
+        test and print('\n'*2, dining_hall.get_text().upper())
+        mealtime_collection = HTMLCollection(dining_hall.get_soup(), 'li', 'preiod-name')
+        for mealtime in mealtime_collection:
+            test and print('\n ', mealtime)
+            station_collection = HTMLCollection(
+                mealtime.get_soup(), 'div', 'cat-name', 
+                key=lambda element: element.get_text() not in HTMLCollection.BANNED_STATIONS
+            )
+            for station in station_collection:
+                test and print('  ', station)
+                food_collection = HTMLCollection(station.get_soup(), 'li', 'recip')
+                for food in food_collection:
+                    test and print('     ', food)
 
 if __name__ == '__main__':
-    URL = 'https://example.html'
+    # URL = 'https://example.html'
     setup(URL, test=True)
